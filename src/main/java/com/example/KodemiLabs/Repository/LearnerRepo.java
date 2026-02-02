4b8364bdb60668619ac1282564f6ba8ddb93c1cd
sarkar <srajsaarkar451@gmail.com>
2026-02-02 15:48:53 +0530

    Repo edit

--- src/main/java/com/example/KodemiLabs/Repository/LearnerRepo.java
+++ src/main/java/com/example/KodemiLabs/Repository/LearnerRepo.java
@@ -1,14 +1,14 @@
 package com.example.KodemiLabs.Repository;
 
-import com.example.KodemiLabs.Model.Learner;
+import com.example.KodemiLabs.Model.User;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 @EnableScan
-public interface LearnerRepo extends CrudRepository<Learner, String> {
+public interface LearnerRepo extends CrudRepository<User, String> {
 
-    List<Learner> findByName(String name);
+    List<User> findByName(String name);
 
 }

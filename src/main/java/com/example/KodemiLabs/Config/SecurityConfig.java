6cb7e9d3d6669420385fa8e444f9bf17b857e920
goivnd78489 <23e51a67e8@hitam.org>
2026-02-02 15:32:28 +0530

    SecurityConfig

---
+++ src/main/java/com/example/KodemiLabs/Config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.KodemiLabs.Config;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/learner/**").hasRole("LEARNER")
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}

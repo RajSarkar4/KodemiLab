e48100ee2ec38011aae88dfd798301ba2d9c8d25
sarkar <srajsaarkar451@gmail.com>
2026-02-02 13:37:30 +0530

    Initial Files

---
+++ src/main/java/com/example/KodemiLabs/KodemiLabsApplication.java
@@ -0,0 +1,13 @@
+package com.example.KodemiLabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodemiLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodemiLabsApplication.class, args);
+	}
+
+}
